4aeebd58ba1349b0670c4d98dafb7171dd7d6994
BugHunter-X <askzenai@gmail.com>
2024-12-11 02:56:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+//Update your tailwind.config.js file
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
